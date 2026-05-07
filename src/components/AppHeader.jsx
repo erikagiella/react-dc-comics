9612da0f6e7612cab2386513c9a412c77858961d
erikagiella <erikagiella93@gmail.com>
2026-05-07 16:10:37 +0200

    Diviso AppHeader in AppLogo e AppNavbar

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,7 +1,11 @@
+import AppLogo from "./AppLogo";
+import AppNavbar from "./AppNavbar";
+
 export default function AppHeader() {
     return (
-        <header>
-
+        <header className="app-header">
+            <AppLogo />
+            <AppNavbar />
         </header>
     )
 }
