9b2346b8ac2630e32c77eddc58a70d2105d4be62
erikagiella <erikagiella93@gmail.com>
2026-05-07 16:11:26 +0200

    componente AppNavbar con lista di link

---
+++ src/components/AppNavbar.jsx
@@ -0,0 +1,19 @@
+export default function AppNavbar() {
+    return (
+        <nav className="navbar">
+            <ul>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+            </ul>
+        </nav>
+    )
+
+}
