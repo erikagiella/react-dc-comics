03a3cf88d4acba5f8b6a714284ef13f4a0e00601
erikagiella <erikagiella93@gmail.com>
2026-05-07 16:10:59 +0200

    componente AppLogo con import immagine del logo

---
+++ src/components/AppLogo.jsx
@@ -0,0 +1,9 @@
+import dclogo from '../assets/img/dc-logo.png'
+
+export default function AppLogo() {
+    return (
+        <div className='nav-logo'>
+            <img src={dclogo} alt="dc-logo" />
+        </div>
+    )
+}
