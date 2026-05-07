eca1c60febfaa5465d237e1bdc80d12e9b76d6b6
erikagiella <erikagiella93@gmail.com>
2026-05-07 17:45:07 +0200

    Creato array links per navbar; inserita funzione map su links, che restituisce gli elementi link già con tag li e a

--- src/components/AppNavbar.jsx
+++ src/components/AppNavbar.jsx
@@ -1,17 +1,27 @@
 export default function AppNavbar() {
+
+    const links = [
+        "CHARACTERS",
+        "COMICS",
+        "MOVIES",
+        "TV",
+        "GAMES",
+        "COLLECTIBLES",
+        "VIDEOS",
+        "FANS",
+        "NEWS",
+        "SHOP"
+    ]
+
     return (
         <nav className="navbar">
             <ul>
-                <li><a href="#"></a></li>
-                <li><a href="#"></a></li>
-                <li><a href="#"></a></li>
-                <li><a href="#"></a></li>
-                <li><a href="#"></a></li>
-                <li><a href="#"></a></li>
-                <li><a href="#"></a></li>
-                <li><a href="#"></a></li>
-                <li><a href="#"></a></li>
-                <li><a href="#"></a></li>
+                {links.map((link) => (
+                    <li key={link}>
+                        <a href="#">{link}</a>
+                    </li>
+                ))
+                }
             </ul>
         </nav>
     )
