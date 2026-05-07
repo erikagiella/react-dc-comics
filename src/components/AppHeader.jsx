a7a8a8d715b14ab4558a93c2d6f2a7643726787e
erikagiella <erikagiella93@gmail.com>
2026-05-07 18:45:13 +0200

    aggiunte classi header flex

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -3,7 +3,7 @@ import AppNavbar from "./AppNavbar";
 
 export default function AppHeader() {
     return (
-        <header className="app-header d-flex">
+        <header className="app-header container d-flex justify-content-between align-items-center py-3">
             <AppLogo />
             <AppNavbar />
         </header>
