1ee5d7926f9cf08654253932184977a71e45933f
erikagiella <erikagiella93@gmail.com>
2026-05-12 22:14:54 +0200

    import immagini del banner

--- src/components/MainBanner.jsx
+++ src/components/MainBanner.jsx
@@ -1,4 +1,15 @@
+import digitalComics from "../assets/img/buy-comics-digital-comics.png"
+import merchandise from "../assets/img/buy-comics-merchandise.png"
+import subscriptions from "../assets/img/buy-comics-subscriptions.png"
+import shopLocator from "../assets/img/buy-comics-shop-locator.png"
+import dcPowerVisa from "../assets/img/buy-dc-power-visa.svg"
+
 export default function MainBanner() {
+    const bannerItems = [
+        {
+            image: 
+        }
+    ]
     return (
         <div className="main-banner py-3">
             <div className="banner-content container p-2">
