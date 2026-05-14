6909cbbbc00b7cb22574e22f3d02bdf9c38e7b22
erikagiella <erikagiella93@gmail.com>
2026-05-14 21:17:52 +0200

    Base struttura array footerSections + .map con collegamento a componente FooterSection

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,7 +1,33 @@
-export default function AppFooter() {
-    return (
-        <footer>
+import FooterSection from "./FooterSection.jsx";
 
+export default function AppFooter() {
+    const footerSections = [
+        {
+            title: "COMISC",
+            links: [
+                {
+                    text: "lalal",
+                    url: ""
+                },
+
+                {
+                    text: "babba",
+                    url: ""
+                },
+            ]
+        },
+    ]
+
+    return (
+
+        <footer>
+            {footerSections.map((section) => (
+                <FooterSection
+                    key={section.title}
+                    title={section.title}
+                    links={section.links}
+                />
+            ))}
         </footer>
     )
 }
