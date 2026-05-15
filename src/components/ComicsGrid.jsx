211d2f24fc803a2a449a406dad01e6ba7577cd97
erikagiella <erikagiella93@gmail.com>
2026-05-15 19:41:49 +0200

    .map su array comics per inserimento ComicCards

--- src/components/ComicsGrid.jsx
+++ src/components/ComicsGrid.jsx
@@ -1,12 +1,16 @@
+import ComicCard from "./ComicCard";
+import comics from "../data/comics.js"
+
 export default function ComicsGrid() {
     return (
         <div className="row">
-            <div className="col-2">
-                <div className="card">
-                    <img className="card-image" src="https://imgs.search.brave.com/aUNyvZBXUulb963JH7KnQm9AMr8bcBoLsiHREOqayIU/rs:fit:612:612:1/g:ce/aHR0cHM6Ly9pNS53/YWxtYXJ0aW1hZ2Vz/LmNvbS9hc3IvOWZm/ZWYzMDMtMGZhYy00/OGRkLTg3ODctYzUy/NTk0MDk2ODAwXzEu/MTc1ZDk1Mjg2NzY0/OGEwOTczMTY2NGMy/MTE1NjNlYWIuanBl/Zz9vZG5XaWR0aD02/MTImb2RuSGVpZ2h0/PTYxMiZvZG5CZz1m/ZmZmZmY"></img>
-                    <div className="card-title">Batman!</div>
-                </div>
-            </div>
+            {comics.map((comic) =>
+            (<ComicCard
+                key={comic.title}
+                title={comic.title}
+                image={comic.thumb}
+            />)
+            )}
         </div>
     )
 }
