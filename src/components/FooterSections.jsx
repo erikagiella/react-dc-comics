2d01afb0a30f493c22bf0c69da668c46a82749fb
erikagiella <erikagiella93@gmail.com>
2026-05-15 18:13:34 +0200

    Inserito il logo dc nel background di footerSections utilizzando il position relative-absolute

--- src/components/FooterSections.jsx
+++ src/components/FooterSections.jsx
@@ -1,26 +1,23 @@
 import FooterSection from "./FooterSection.jsx";
 import footerSections from "../data/FooterLinks.js";
+import footerBackgroundLogo from "../assets/img/dc-logo-bg.png"
 
 export default function FooterSections() {
     return (
-        <div className="footer-sections">
-            <div className="container d-flex gap-5">
-                <div className="row">
-                    <div className="col-5 d-flex flex-column gap-3">
-                        <FooterSection {...footerSections[0]} />
-                        <FooterSection {...footerSections[1]} />
-                    </div>
-
-                    <div className="col-4">
-                        <FooterSection {...footerSections[2]} />
-                    </div>
-
-                    <div className="col-3">
-                        <FooterSection {...footerSections[3]} />
-                    </div>
-
+        <div className="footer-sections container d-flex gap-5">
+            <div className="row">
+                <div className="col-5 d-flex flex-column gap-3">
+                    <FooterSection {...footerSections[0]} />
+                    <FooterSection {...footerSections[1]} />
                 </div>
 
+                <div className="col-4">
+                    <FooterSection {...footerSections[2]} />
+                </div>
+
+                <div className="col-3">
+                    <FooterSection {...footerSections[3]} />
+                </div>
             </div>
         </div>
     )
