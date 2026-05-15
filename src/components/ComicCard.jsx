a91043ba9abc52f747ed35110556ccf82b4630e4
erikagiella <erikagiella93@gmail.com>
2026-05-15 21:14:02 +0200

    Stilizzata card

--- src/components/ComicCard.jsx
+++ src/components/ComicCard.jsx
@@ -1,8 +1,8 @@
 export default function ComicCard({ title, image }) {
     return (
         <div className="comic-card col-2">
-            <img className="card-image" src={image}></img>
-            <div className="card-title">{title}</div>
+            <img className="comic-card-image mb-2" src={image}></img>
+            <div className="comic-card-title text-uppercase">{title}</div>
         </div>
     )
 }
