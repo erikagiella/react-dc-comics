e61abd004795401e2447f4f0aac117f448dd4b37
erikagiella <erikagiella93@gmail.com>
2026-05-15 21:14:17 +0200

    stilizzazione grid/card

--- src/components/ComicsGrid.jsx
+++ src/components/ComicsGrid.jsx
@@ -3,7 +3,7 @@ import comics from "../data/comics.js"
 
 export default function ComicsGrid() {
     return (
-        <div className="row">
+        <div className="row g-3">
             {comics.map((comic) =>
             (<ComicCard
                 key={comic.title}
