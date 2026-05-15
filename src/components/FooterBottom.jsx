7b7c48f8128bf1442b9d3eef5cadfdb90c78d921
erikagiella <erikagiella93@gmail.com>
2026-05-15 18:20:17 +0200

    stilizzazione footer bottom

--- src/components/FooterBottom.jsx
+++ src/components/FooterBottom.jsx
@@ -4,8 +4,8 @@ import SocialSection from "./SocialSection"
 
 export default function FooterBottom() {
     return (
-        <div className="footer-bottom">
-            <div className="container d-flex justify-content-between">
+        <div className="footer-bottom py-4">
+            <div className="container d-flex justify-content-between align-items-center">
                 <SignUpButton />
                 <SocialSection />
             </div>
--- src/components/SocialSection.jsx
+++ src/components/SocialSection.jsx
@@ -2,9 +2,9 @@ import socialLinks from "../data/socialLinks"
 
 export default function SocialSection() {
     return (
-        <div className="social-section">
-            <h4>FOLLOW US</h4>
-            <ul>
+        <div className="social-section d-flex">
+            <h4 className="social-followus">FOLLOW US</h4>
+            <ul className="d-flex">
                 {socialLinks.map((social) => (
                     <li key={social.name}>
                         <a href={social.url}>
