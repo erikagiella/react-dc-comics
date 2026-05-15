7b7242741cd7597e829f2b18d478d58afb84efd8
erikagiella <erikagiella93@gmail.com>
2026-05-15 19:31:35 +0200

    Componente ComicsGrid

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,14 +1,9 @@
+import ComicsGrid from "./ComicsGrid";
+
 export default function MainContent() {
     return (
         <div className="main-content container py-5">
-            <div className="row">
-                <div className="col-2">
-                    <div className="card">
-                        <img className="card-image" src="https://imgs.search.brave.com/aUNyvZBXUulb963JH7KnQm9AMr8bcBoLsiHREOqayIU/rs:fit:612:612:1/g:ce/aHR0cHM6Ly9pNS53/YWxtYXJ0aW1hZ2Vz/LmNvbS9hc3IvOWZm/ZWYzMDMtMGZhYy00/OGRkLTg3ODctYzUy/NTk0MDk2ODAwXzEu/MTc1ZDk1Mjg2NzY0/OGEwOTczMTY2NGMy/MTE1NjNlYWIuanBl/Zz9vZG5XaWR0aD02/MTImb2RuSGVpZ2h0/PTYxMiZvZG5CZz1m/ZmZmZmY"></img>
-                        <div className="card-title">Batman!</div>
-                    </div>
-                </div>
-            </div>
+            <ComicsGrid />
         </div>
     )
 }
