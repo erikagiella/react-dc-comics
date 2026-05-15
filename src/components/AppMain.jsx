2709dc57cd87204a114fe9fb59bfca75dd33946f
erikagiella <erikagiella93@gmail.com>
2026-05-15 19:19:05 +0200

    Creato Jumbotron

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -4,6 +4,7 @@ import MainContent from "./MainContent";
 export default function AppMain() {
     return (
         <main className="app-main">
+            <div className="dc-jumbotron"></div>
             <MainContent />
             <MainBanner />
         </main>
